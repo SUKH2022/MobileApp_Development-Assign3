e223ef7549978ab1f5f1f810a417861d52664404
Sukhpreet Saini <119287107+SUKH2022@users.noreply.github.com>
2025-03-14 15:03:09 -0400

    Exercise2

---
+++ Sukhpreet-Saini_COMP2125-Sec001_Lab03-Exercise2/Sukhpreet-Saini_COMP2125-Sec001_Lab03-Exercise2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sukhpreet-Saini_COMP2125-Sec001_Lab03-Exercise2
+//
+//  Created by Sukhpreet Saini on 2025-03-14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Sukhpreet-Saini_COMP2125-Sec001_Lab03-Exercise2/Sukhpreet-Saini_COMP2125-Sec001_Lab03-Exercise2Tests/Sukhpreet_Saini_COMP2125_Sec001_Lab03_Exercise2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Sukhpreet_Saini_COMP2125_Sec001_Lab03_Exercise2Tests.swift
+//  Sukhpreet-Saini_COMP2125-Sec001_Lab03-Exercise2Tests
+//
+//  Created by Sukhpreet Saini on 2025-03-14.
+//
+
+import Testing
+@testable import Sukhpreet_Saini_COMP2125_Sec001_Lab03_Exercise2
+
+struct Sukhpreet_Saini_COMP2125_Sec001_Lab03_Exercise2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
